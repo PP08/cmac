8cc42f41a1b41cc28197aee3da27d24fbef224c1
Lance Lot <phucphuong1293@gmail.com>
2017-12-05 23:01:51 +0700

    Mod: Collect category

--- cmac/pipelines.py
+++ cmac/pipelines.py
@@ -9,10 +9,16 @@ from pymongo import MongoClient
 client = MongoClient()
 db = client.database
 table = db.macapps
+collection = db.category
 
 class CmacPipeline(object):
     def process_item(self, item, spider):
         dict_item = dict(item)
-        if table.find({'name': dict_item["name"]}).count() == 0:
-            table.insert_one(dict_item)
+        # if table.find({'name': dict_item["name"]}).count() == 0:
+        #     table.insert_one(dict_item)
+        # return item
+        #
+        if collection.find({'name': dict_item["category"]}).count() == 0:
+            tmp = {"name": dict_item["category"]}
+            collection.insert_one(tmp)
         return item
